09f3f1e82ead3335f49215545b2ca6feb4da1143
justinjames92 <justinjames92@gmail.com>
2023-03-19 15:45:40 -0400

    intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// @flow strict
+
+import * as React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default Navbar;
